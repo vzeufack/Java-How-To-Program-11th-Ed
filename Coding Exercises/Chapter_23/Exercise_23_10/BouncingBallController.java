7d7f0900a6c1cfaa507f3f1b81d604b053b025c6
Vannel Zeufack <zvanel@live.fr>
2023-11-30 20:04:17 -0500

    Exercises 23_10 & 23_11

---
+++ Coding Exercises/Chapter_23/Exercise_23_10/BouncingBallController.java
@@ -0,0 +1,55 @@
+// BallAnimationTimerController.java
+// Bounce a circle around a window using an AnimationTimer subclass. 
+import java.security.SecureRandom;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class BouncingBallController {
+   @FXML private Circle c;
+   @FXML private Pane pane;
+   
+   public void initialize() {
+      c.setVisible(false);
+   }
+
+   @FXML
+   void generateBall(MouseEvent event) {
+      SecureRandom random = new SecureRandom();
+      Color color = new Color(random.nextDouble(), random.nextDouble(), random.nextDouble(), random.nextDouble());
+      Circle newCircle = new Circle(c.getRadius(), color);
+      pane.getChildren().add(newCircle);
+      newCircle.setStroke(c.getStroke());
+      newCircle.setStrokeWidth(c.getStrokeWidth());
+      newCircle.setVisible(true);
+      newCircle.setLayoutX(event.getSceneX());
+      newCircle.setLayoutY(event.getSceneY());
+      GenerateBallTask task = new GenerateBallTask(newCircle, pane.getBoundsInLocal());
+
+      ExecutorService executorService = Executors.newFixedThreadPool(1);
+      executorService.execute(task);
+      executorService.shutdown();
+   }
+}
+
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
---
+++ Coding Exercises/Chapter_23/Exercise_23_10/GenerateBallTask.java
@@ -0,0 +1,62 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Bounds;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
+import java.security.SecureRandom;
+
+public class GenerateBallTask extends Task {
+    private SecureRandom random = new SecureRandom();
+    private Circle c;
+    private Bounds bounds;
+
+    public GenerateBallTask(Circle circle, Bounds bounds) {
+        this.bounds = bounds;
+        this.c = circle;
+    }
+
+    private boolean hitRightOrLeftEdge(Bounds bounds) {
+        return (c.getLayoutX() <= (bounds.getMinX() + c.getRadius())) ||
+                (c.getLayoutX() >= (bounds.getMaxX() - c.getRadius()));
+    }
+
+    // determines whether the circle hit top/bottom of the window
+    private boolean hitTopOrBottom(Bounds bounds) {
+        return (c.getLayoutY() <= (bounds.getMinY() + c.getRadius())) ||
+                (c.getLayoutY() >= (bounds.getMaxY() - c.getRadius()));
+    }
+
+    @Override
+    protected Object call() throws Exception {
+        Timeline timelineAnimation = new Timeline(
+                new KeyFrame(Duration.millis(10),
+                        new EventHandler<ActionEvent>() {
+                            int dx = 1 + random.nextInt(5);
+                            int dy = 1 + random.nextInt(5);
+
+                            // move the circle by the dx and dy amounts
+                            @Override
+                            public void handle(final ActionEvent e) {
+                                c.setLayoutX(c.getLayoutX() + dx);
+                                c.setLayoutY(c.getLayoutY() + dy);
+                                if (hitRightOrLeftEdge(bounds)) {
+                                    dx *= -1;
+                                }
+
+                                if (hitTopOrBottom(bounds)) {
+                                    dy *= -1;
+                                }
+                            }
+                        }
+                )
+        );
+
+        timelineAnimation.setCycleCount(Timeline.INDEFINITE);
+        timelineAnimation.play();
+        return null;
+    }
+}
